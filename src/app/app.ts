a6aca7f7085ef20ff610ecf7dc9158d822cb5679
lorinsteiner <lorin.steiner@bluewin.ch>
2025-06-27 22:56:32 +0200

    style: adjust styles of footer, header and whole page, finish start page

--- src/app/app.ts
+++ src/app/app.ts
@@ -1,11 +1,13 @@
 import { Component } from '@angular/core';
 import {Start} from './start/start';
+import {RouterOutlet} from '@angular/router';
 
 @Component({
   selector: 'app-root',
   templateUrl: './app.html',
   imports: [
-    Start
+    Start,
+    RouterOutlet
   ],
   styleUrls: ['./app.css']
 })
